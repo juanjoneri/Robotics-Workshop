6dbd09fccd807851266556dddd0d105c031c2e71
Juan <juanjo.neri@gmail.com>
2016-08-01 23:32:59 -0300

    ds4 essentials

--- ds4.py
+++ ds4.py
@@ -1,13 +1,9 @@
 import pygame
-import RPi.GPIO as GPIO
 
-# Define some colors
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 
-# This is a simple class that will help us print to the screen
-# It has nothing to do with the joysticks, just outputing the
-# information.
+# Class to output to the scren
 class TextPrint:
     def __init__(self):
         self.reset()
@@ -17,172 +13,65 @@ class TextPrint:
         textBitmap = self.font.render(textString, True, BLACK)
         screen.blit(textBitmap, [self.x, self.y])
         self.y += self.line_height
-        
+
     def reset(self):
         self.x = 10
         self.y = 10
         self.line_height = 15
-        
+
     def indent(self):
         self.x += 10
-        
+
     def unindent(self):
         self.x -= 10
-    
 
+# pygame rubish
 pygame.init()
- 
-# Set the width and height of the screen [width,height]
-size = [500, 700]
+size = [360, 120]
 screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("My Game")
-
-#Loop until the user clicks the close button.
+pygame.display.set_caption("Adrometeo")
 done = False
-
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
-
-# Initialize the joysticks
 pygame.joystick.init()
-    
-# Get ready to print
 textPrint = TextPrint()
 
-################NUESTRO CODIGO
-GPIO.setmode(GPIO.BOARD)
-    
-GPIO.setup(15, GPIO.OUT)
-motorR = GPIO.PWM(15, 60)
-motorR.start(0)
-
-GPIO.setup(13, GPIO.OUT)
-motorL = GPIO.PWM(13, 60)
-motorL.start(0)
-################FIN NUESTRO CODIGO
-
-# -------- Main Program Loop -----------
+# -------- WHERE THE MAGIC HAPPENS ----------- #
 while done==False:
-    motorR = GPIO.PWM(15, 60)
-    motorL = GPIO.PWM(13, 60)
     # EVENT PROCESSING STEP
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done=True # Flag that we are done so we exit this loop
-        
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        if event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
-            
- 
+
     # DRAWING STEP
-    # First, clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
     screen.fill(WHITE)
     textPrint.reset()
 
-    # Get count of joysticks
-    joystick_count = pygame.joystick.get_count()
+    #Joystick setup
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    name = joystick.get_name()
+    textPrint.print(screen, name )
 
-    textPrint.print(screen, "Number of joysticks: {}".format(joystick_count) )
+    # Axes
+    textPrint.print(screen, "Axes" )
+    textPrint.indent()
+
+    axisL = joystick.get_axis( 1 )
+    axisR = joystick.get_axis( 2 )
+    textPrint.print(screen, "Axis L value: {:>6.3f}".format(axisL))
+    textPrint.print(screen, "Axis R value: {:>6.3f}".format(axisR)) 
+    textPrint.unindent()
+
+    # Buttons
+    textPrint.print(screen, "Buttons" )
     textPrint.indent()
     
-    # For each joystick:
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-    
-        textPrint.print(screen, "Joystick {}".format(i) )
-        textPrint.indent()
-    
-        # Get the name from the OS for the controller/joystick
-        name = joystick.get_name()
-        textPrint.print(screen, "Joystick name: {}".format(name) )
-        
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
-        axes = joystick.get_numaxes()
-        textPrint.print(screen, "Number of axes: {}".format(axes) )
-        textPrint.indent()
+    button0 = joystick.get_button( 2 )
+    textPrint.print(screen, "Button 0 value: {}".format(button0))
+    textPrint.unindent()
 
-        
-        
-        for i in range( axes ):
-            axis = joystick.get_axis( i )
-            textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis) )
-        textPrint.unindent()
-
-        ################NUESTRO CODIGO
-        axisLeft = -joystick.get_axis( 1 )
-        axisRight = joystick.get_axis( 2 ) #cambiar si no rinde
-        
-        global baseDuty
-        
-        if axisLeft <= 0:
-            baseDuty= 0
-        else :
-            baseDuty= axisLeft*30
-        print(baseDuty)
-        motorL.ChangeDutyCycle(baseDuty)
-        motorR.ChangeDutyCycle(baseDuty)
-
-        if axisRight != 0:
-            if axisRight >0 :
-                motorL.ChangeDutyCycle(baseDuty+axisRight*10)
-                print(baseDuty+axisRight*10)
-            else :
-                motorR.ChangeDutyCycle(baseDuty-axisRight*10)
-                print(baseDuty-axisRight*10)
-        
-        ###############FIN NUESTRO CODIGO
-                
-        buttons = joystick.get_numbuttons()
-        textPrint.print(screen, "Number of buttons: {}".format(buttons) )
-        textPrint.indent()
-
-        for i in range( buttons ):
-            button = joystick.get_button( i )
-            textPrint.print(screen, "Button {:>2} value: {}".format(i,button) )
-        textPrint.unindent()
-
-        ################NUESTRO CODIGO
-        buttonO = joystick.get_button( 2 )
-        if buttonO == 1:
-            done = True
-        ###############FIN NUESTRO CODIGO
-        
-        
-            
-        # Hat switch. All or nothing for direction, not like joysticks.
-        # Value comes back in an array.
-        hats = joystick.get_numhats()
-        textPrint.print(screen, "Number of hats: {}".format(hats) )
-        textPrint.indent()
-
-        for i in range( hats ):
-            hat = joystick.get_hat( i )
-            textPrint.print(screen, "Hat {} value: {}".format(i, str(hat)) )
-        textPrint.unindent()
-        
-        textPrint.unindent()
-
-    
-    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-    
-    # Go ahead and update the screen with what we've drawn.
+    # S'more pygame magic
     pygame.display.flip()
-
-    # Limit to 20 frames per second
     clock.tick(20)
-    
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-print('killing')
-motorL.stop()
-motorR.stop()
-GPIO.cleanup()
+
 pygame.quit ()
